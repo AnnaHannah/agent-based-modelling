809a46c68243fb3d0983ef9cfbdcb335f923063b
AnnaHannah <38104875+AnnaHannah@users.noreply.github.com>
2021-11-19 11:17:56 +0100

    Update installer_do_first.R

--- my_files/installer_do_first.R
+++ my_files/installer_do_first.R
@@ -1,19 +1,14 @@
-# Everytime rstudio opens new slow fucking  session ... 
-# i need to install odd packes here ...
+# Everytime rstudio opens new slow session ... 
+# i need to install packes ...
 
-
-
-# Error+Messages prevention
+# Error+Messages suppression
 be_quiet <- suppressPackageStartupMessages
 
-## --------------------local package.check --------------------------------------------------
-
+## --------------------easy package install --------------------------------------------------
 
 packages <- c("rgeos", "plyr", "ggplot2" )
 
-## Now load or install local pakages:
-
-
+## Now load or install CRAN-pakages:
 package.check <- lapply(
   packages,
   FUN = function(x) {
@@ -24,14 +19,15 @@ package.check <- lapply(
   }
 )
 
-## --------------------global package.check -------------------------------------------------
+## -------------------- global packages / work arround cran (most common r libary) -------------------------------------------------
 
 #install.packages("https://github.com/leerichardson/spew.git", dependencies = TRUE)
 install.packages("https://cran.r-project.org/src/contrib/Archive/foreign/foreign_0.8-76.tar.gz", dependencies=TRUE)
 install.packages("rtools", c="https://cran.r-project.org/bin/windows/Rtools")
 
-## ---------------------extra attention package.check -------------------------------------------------
-# win.binary tools repo tools machen dependencies nicht korrekt
+## --------------------- extra attention packages -------------------------------------------------
+
+# win.binary tools because repo-tools dont get dependencies right 
 
 write('PATH="${RTOOLS40_HOME}\\usr\\bin;${PATH}"', file = "~/.Renviron", append = TRUE)
 Sys.which("make")
@@ -39,15 +35,14 @@ install.packages("devtools", type = "win.binary", dependencies=TRUE)
 install.packages("maptools", type = "win.binary", dependencies=TRUE)
 #install.packages("spew", type = "win.binary", dependencies=TRUE)
 
-
+# search printes all fundamental installed packages
 writeLines("\n--- installed! ---\n")
-# search printes all 
 print(search())
 
+## --------------------- Manual option: insert packages -------------------------------------------------
 
-# Manual option:
 # extra packages (tools->install Packages (git-repo)) 
 
-# Maybe windowsproblems
+# Maybe windowsproblems (Enviromental Problems in RStudio):
 ## write('PATH="${RTOOLS40_HOME}\\usr\\bin;${PATH}"', file = "~/.Renviron", append = TRUE) #
 
